a54891262cd6267066e253bfe8470d36d19d132b
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-02-27 11:39:08 -0800

    Platform strip movement

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -7,25 +7,32 @@ using Random = UnityEngine.Random;
 public class PlatformSpawner : MonoBehaviour
 {
 
-    // public GameObject platformObj;
     public GameObject platformBlockPrefab;
     public int platformsSpawned;
 
-    int levelWidth = 10;
-    float TopLevelYPos = 2.2f;
-    float bottomLevelYPos = -3.2f;
     double blockPrefabWidth;
+    int levelWidth = 10;
+    float topLevelYPos = 2.2f;
+    float bottomLevelYPos = -3.2f;
+
+    GameObject platformObj;
+    GameObject playerObj;
     List<GameObject> tileTopRow;
     List<GameObject> tileBottomRow;
+
+    bool shouldStartMoving = false;
+    int tileMoveSpeed = 4;
 
     // Start is called before the first frame update
     void Start()
     {
         platformsSpawned = 0;
-        // platformObj = GameObject.Find("Platform");
+        this.platformObj = GameObject.Find("Platform");
+        this.playerObj = GameObject.Find("Player");
         Renderer blockPrefabRenderer = platformBlockPrefab.GetComponent<Renderer>();
         blockPrefabWidth = blockPrefabRenderer.bounds.size.x;
         tileBottomRow = new List<GameObject>();
+        tileTopRow = new List<GameObject>();
 
 
         print("block prefab width: " + blockPrefabWidth);
@@ -35,7 +42,8 @@ public class PlatformSpawner : MonoBehaviour
         {
             if (i == 4 || i == 5) 
                 continue;
-            GameObject tileTop = createTile(i, TopLevelYPos);
+            GameObject tileTop = createTile(i, topLevelYPos);
+            tileTopRow.Add(tileTop);
         }
 
         generateNewLevel();
@@ -43,21 +51,40 @@ public class PlatformSpawner : MonoBehaviour
 
     void Update()
     {
-        moveUp();
+        // if (Input.GetButtonDown("Jump")) 
+        if (playerObj.transform.position.y <= bottomLevelYPos)
+        {
+            this.shouldStartMoving = true;
+        }
+
+        if (shouldStartMoving) {
+            moveUp();
+        }
     }
 
     void moveUp() 
     {
-        if (Input.GetButtonDown("Jump")) 
+
+        float step = tileMoveSpeed * Time.deltaTime; // calculate distance to move
+
+        foreach (GameObject tile in tileTopRow) 
         {
-            foreach (GameObject tile in tileBottomRow) 
-            {
-                Rigidbody2D tileBody = tile.GetComponent<Rigidbody2D>();
-                tileBody.velocity = new Vector2(0, 10);
-                // tileBody.gravityScale = 0;
-                // tileBody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-            }
+            tile.transform.Translate (Vector3.up * Time.deltaTime * tileMoveSpeed); 
         }
+        foreach (GameObject tile in tileBottomRow) 
+        {
+            tile.transform.Translate (Vector3.up * Time.deltaTime * tileMoveSpeed); 
+        }
+
+        // bottom row passed a certain height
+        // * change to triggerBlock instead of bottomRow
+        if (tileBottomRow[0].transform.position.y >= topLevelYPos) {
+            this.shouldStartMoving = false;
+            // set new triggerBlock block
+            // generateNewLevel's
+            // destroy all blocks out of window (only the top ones)
+        }
+
     }
 
     void generateNewLevel() {
@@ -79,13 +106,7 @@ public class PlatformSpawner : MonoBehaviour
         double xPos = startingPoint + (blockPrefabWidth) * xBlock;
         Vector3 tilePosition = new Vector3(Convert.ToSingle(xPos), yPos, 0);
         GameObject tile = (GameObject) Instantiate(platformBlockPrefab, tilePosition, Quaternion.identity);
-        Rigidbody2D tileBody = tile.GetComponent<Rigidbody2D>();
-        tileBody.velocity = Vector2.zero;
-        tileBody.gravityScale = 0;
-        tileBody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-
-        tileBody.isKinematic = false;
-
+        tile.transform.parent = this.platformObj.transform;
         return tile;
     }
 }
