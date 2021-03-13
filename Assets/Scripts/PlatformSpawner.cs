6bd6f1e1b6c8e01d665bada1c0e8b6d88bb7246f
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-03-13 11:19:13 -0800

    Change to new platform movement method, randomizer needs to be improved

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -14,12 +14,12 @@ public class PlatformSpawner : MonoBehaviour
     public GameObject platformBlockPrefab;
     public int platformsSpawned;
 
-    int levelWidth = 10;
+    int levelWidth = 4;
     float topLevelYPos = 2.2f;
     float bottomLevelYPos = -3.2f;
     float undergroundLevelYPos = -8.2f;
     float levelHeightDifference = 5.0f;
-    float blockSpawnRate = 0.3f;
+    float blockSpawnRate = 0.2f;
 
     double blockPrefabWidth;
     float playerHeight;
@@ -29,69 +29,61 @@ public class PlatformSpawner : MonoBehaviour
     List<GameObject> tileTopRow;
     List<GameObject> allTiles;
 
-    GameObject triggerBlock;
-    GameObject oldTriggerBlock;
-
-    bool shouldStartMoving = false;
-    float tileMoveSpeed = 4.5f;
+    float levelMoveSpeed = 3.5f;
+    float levelSpawnRateModifier = 0.3f;
 
     // Start is called before the first frame update
     void Start()
     {
+        print("Screen width: " + Screen.width);
         platformsSpawned = 0;
         this.platformObj = GameObject.Find("Platform");
         this.playerObj = GameObject.Find("Player");
         this.playerHeight = this.playerObj.GetComponent<Renderer>().bounds.size.y;
         Renderer blockPrefabRenderer = platformBlockPrefab.GetComponent<Renderer>();
         blockPrefabWidth = blockPrefabRenderer.bounds.size.x;
+        // levelWidth = (int) (Screen.width / blockPrefabWidth);
+        // print("level width: " + levelWidth);
+
         allTiles = new List<GameObject>();
 
         generateNewLevel(topLevelYPos);
         generateNewLevel(bottomLevelYPos);
+
+        InvokeRepeating("generateUndergroundLevel", 0f, levelMoveSpeed * levelSpawnRateModifier);
     }
 
     void Update()
     {
+        moveUp();
+    }
 
-        if (this.triggerBlock) {
-            float triggerBlockHeight = this.triggerBlock.GetComponent<Renderer>().bounds.size.y;
-            float triggerBlockTop = this.triggerBlock.transform.position.y + triggerBlockHeight / 2;
-
-            if (!shouldStartMoving && playerObj.transform.position.y - playerHeight / 2 <= triggerBlockTop)
-            {
-                this.oldTriggerBlock = this.triggerBlock;
-                generateNewLevel(undergroundLevelYPos);
-                this.shouldStartMoving = true;
-            }
-        }
-
-
-        if (shouldStartMoving) {
-            moveUp();
-        }
+    void generateUndergroundLevel()
+    {
+        generateNewLevel(undergroundLevelYPos);
     }
 
     void moveUp() 
     {
-
-        float step = tileMoveSpeed * Time.deltaTime; // calculate distance to move
+        float step = levelMoveSpeed * Time.deltaTime; // calculate distance to move
         foreach (GameObject tile in allTiles) 
         {
-            tile.transform.Translate (Vector3.up * Time.deltaTime * tileMoveSpeed); 
+            tile.transform.Translate (Vector3.up * Time.deltaTime * levelMoveSpeed); 
         }
-
-        // triggerBlock passed a certain height
-        if (this.oldTriggerBlock.transform.position.y >= topLevelYPos) {
-            this.shouldStartMoving = false;
-            // destroy all blocks out of window (only the top ones)
-        }
-
     }
 
     void generateNewLevel(float height) {
         // int carveGapStartBlock = Random.Range(0, levelWidth - 1);
-
         // randomly adjust height between bottom level and underground
+        // if ((i == carveGapStartBlock) || (i == carveGapStartBlock + 1)) 
+        // {
+        //     continue;
+        // }
+        // GameObject tile = createTile(i, height);
+
+        // int spawnXBlocks = (int) (Random.Range(1, levelWidth - 1));
+        // float initialGap = Random.Range(0, spawnXBlocks * blockPrefabWidth);
+
         for (int i = 0; i < levelWidth; i++) 
         {
             bool shouldSpawn = (Random.Range(0, 10) <= this.blockSpawnRate * 10);
@@ -103,30 +95,26 @@ public class PlatformSpawner : MonoBehaviour
             float maxHeight = height + this.levelHeightDifference / 2;
             float randomHeight = Random.Range(height, maxHeight);
             GameObject tile = createTile(i, randomHeight);
-            //
-            // if ((i == carveGapStartBlock) || (i == carveGapStartBlock + 1)) 
-            // {
-            //     continue;
-            // }
-            // GameObject tile = createTile(i, height);
+            
             allTiles.Add(tile);
         }
-        this.triggerBlock = allTiles[allTiles.Count - 1];
     }
 
     GameObject createTile(int xBlock, float yPos) 
     {
-        double startingPoint = blockPrefabWidth * (levelWidth / 2) * (-1);
+        // double startingPoint = blockPrefabWidth * (levelWidth / 2) * (-1);
+        double startingPoint = Camera.main.ViewportToWorldPoint(new Vector3(0,1,0)).x;
+        // double startingPoint = 0;
         double xPos = startingPoint + (blockPrefabWidth) * xBlock;
         Vector3 tilePosition = new Vector3(Convert.ToSingle(xPos), yPos, 0);
         GameObject tile = (GameObject) Instantiate(platformBlockPrefab, tilePosition, Quaternion.identity);
-        // tile.transform.localscale.y = 10;
 
+        // tile.transform.localscale.y = 10;
         // RectTransform tileRT = tile.GetComponent<RectTransform>();
         // tileRT.sizeDelta = new Vector2(1, 10);
         // tile.RectTransform.sizeDelta = new Vector2(1, 2);
+        
         tile.transform.SetParent(this.platformObj.transform);
-
         return tile;
     }
 }
