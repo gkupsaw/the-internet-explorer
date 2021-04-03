c77089943eefd8bb65dea625828bb73141b199a4
gkupsaw <griffin_kupsaw@brown.edu>
2021-04-03 16:12:52 -0400

    add glitch destroyed fx

--- Assets/Scripts/Ads/AdMovement.cs
+++ Assets/Scripts/Ads/AdMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AdMovement : MonoBehaviour
 {
+    public AudioClip glitchDestroyedSound;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (Input.GetMouseButton(0) &&
             col.gameObject.tag == "WalkingGlitch" || col.gameObject.tag == "TeleportingGlitch")
         {
+            AudioSource.PlayClipAtPoint(glitchDestroyedSound, new Vector3(0,0,0));
             Destroy(col.gameObject);
             Destroy(gameObject);
         }
