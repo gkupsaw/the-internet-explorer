048344fde30317224f7bcd3911cd35be7e87b3ef
gkupsaw <griffin_kupsaw@brown.edu>
2021-04-10 19:16:37 -0400

    add LB script

---
+++ Assets/Scripts/System/Leaderboard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour
+{
+    public Text name;
+    public Text score;
+    // use Application.persistentDataPath ?
+    private string _filepath = "./Data/Leaderboard.json";
+
+    public void Save()
+    {
+        Dictionary<string, string> currLeaderboard = Load();
+        currLeaderboard.Add(name.text, score.text);
+        System.IO.File.WriteAllText(_filepath, JsonUtility.ToJson(currLeaderboard));
+    }
+
+    Dictionary<string, string> Load()
+    {
+        using (StreamReader r = new StreamReader(_filepath))
+        {
+            string json = r.ReadToEnd();
+            Dictionary<string, string> items = JsonUtility.FromJson<Dictionary<string, string>>(json);
+            return items;
+        }
+    }
+}
