be84fd48202617d6d7be54bfb59b922c22506a4c
gkupsaw <griffin_kupsaw@brown.edu>
2021-04-11 17:36:25 -0400

    write data serializer/deserializer, probs overkill

--- Assets/Scripts/System/Leaderboard.cs
+++ Assets/Scripts/System/Leaderboard.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+// disallowed chars: {}\",:
+
 public class Leaderboard : MonoBehaviour
 {
-    public TMP_Text playername;
-    public TMP_Text score;
+    public TMP_Text _playername;
+    public TMP_Text _score;
     // use Application.persistentDataPath ?
     private string _filepath = "./Data/Leaderboard.json";
 
@@ -20,19 +22,65 @@ public class Leaderboard : MonoBehaviour
     public void Save()
     {
         Dictionary<string, string> currLeaderboard = Load();
-        currLeaderboard.Add(playername.text, score.text);
-        Debug.Log(currLeaderboard.Get(playername.text));
-        Debug.Log(JsonUtility.ToJson(currLeaderboard));
-        System.IO.File.WriteAllText(_filepath, JsonUtility.ToJson(currLeaderboard));
+
+        string playername = _playername.text.Trim();
+        string score = _score.text.Trim();
+        if (currLeaderboard.ContainsKey(playername))
+        {
+            currLeaderboard.Remove(playername);
+        }
+        currLeaderboard.Add(playername, score);
+
+        System.IO.File.WriteAllText(_filepath, Serialize(currLeaderboard));
     }
 
     Dictionary<string, string> Load()
     {
         using (StreamReader r = new StreamReader(_filepath))
         {
-            string json = r.ReadToEnd();
-            Dictionary<string, string> items = JsonUtility.FromJson<Dictionary<string, string>>(json);
-            return items;
+            string data = r.ReadToEnd();
+            Dictionary<string, string> currLeaderboard = Deserialize(data);
+            return currLeaderboard;
         }
+    }
+
+    Dictionary<string, string> Deserialize(string data)
+    {
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+        string[] nameScores = data.Substring(1, data.Length - 2).Split(',');
+
+        foreach (string nameScore in nameScores)
+        {
+            string[] ns = nameScore.Split(':');
+
+            if (ns.Length == 1)
+            {
+                return dict;
+            }
+
+            dict.Add(ns[0].Replace('"', ' ').Trim(), ns[1].Replace('"', ' ').Trim());
+        }
+
+        return dict;
+    }
+
+    string Serialize(Dictionary<string, string> dict)
+    {
+        string res = "";
+        int i = 0;
+        int sz = dict.Keys.Count;
+        foreach(KeyValuePair<string, string> kvp in dict)
+        {
+            string playername = kvp.Key.Trim();
+            string score = kvp.Value.Trim();
+            res += "\"" + playername + "\"" + " : " + "\"" + score + "\"";
+            if (i < sz - 1)
+            {
+                res += ",";
+            }
+            res += "\n";
+            i++;
+        }
+        return "{\n" + res + "}";
     }
 }
